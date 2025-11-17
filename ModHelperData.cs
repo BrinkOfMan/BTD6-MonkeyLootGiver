f999c323867c5c2a6f319d837e3ee8a88b0de459
Ethan Brinkman <brinkmanethan@gmail.com>
2025-11-16 19:32:22 -0600

    Changing helperdata version and build.yml project name

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,8 +2,8 @@ namespace MonkeyLootGiver;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "50.0";
-    public const string Version = "1.0.0";
+    public const string WorksOnVersion = "34.0";
+    public const string Version = "1.0.3";
     public const string Name = "Gain Monkey Loot After Round 100";
 
     public const string Description = "Gain various Monkey Loot! Configurable in mod settings. Works in single-player and co-op.";
